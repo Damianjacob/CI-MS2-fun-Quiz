3fb5e9158d381ac65da8d696d0c3c15cc3c98d41
Damian Jacob <damianm.jacob@yahoo.com>
2021-06-29 08:01:18 +0100

    Add empty functions to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,55 @@
-function test(){
-    document.body.style.backgroundColor = 'blue'
+// This is where all the questions are stored
+
+let geographyQuestions = {
+
+}
+
+let scienceQuestions = {
+
+}
+
+let musicQuestions = {
+
+}
+
+let animalsQuestions = {
+
+}
+
+/**runs the game */
+function runQuiz(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function diminishLifePoints(){
+
+}
+
+function gameOver(){
+
+}
+
+function displayGeographyQuestion(){
+
+}
+
+function displayScienceQuestion(){
+
+}
+
+function displayMusicQuestion(){
+
+}
+
+function displayAnimalsQuestion(){
+
 }
 
